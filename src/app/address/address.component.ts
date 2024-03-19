092d312d98ec405697e0f975de7878bcb0fabc64
Ramen96 <eric.worsfold@gmail.com>
2024-03-18 22:57:58 -0400

    created new component to fetch blockchain data on eth

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -11,7 +11,7 @@ import { FooterComponent } from '../footer/footer.component';
   template:`
     <div class="flex justify-center items-center h-1/2 mt-36">
         <div class="w-full max-w-4xl h-80 bg-slate-100 rounded-md overflow-hidden shadow-xl shadow-slate-300 p-4 dark:bg-zinc-900 dark:shadow-slate-900 dark:shadow-lg dark:text-white">
-            <h2 class="text-3xl text-center font-extralight tracking-widest mb-4 text-black dark:text-gray-200">Address</h2>
+            <h2 class="text-3xl text-center font-extralight tracking-widest mb-4 text-black dark:text-gray-200">Address/Contract</h2>
             <form class="flex items-center flex-col max-w-lg mx-auto mb-4 space-y-10">   
                 <div class="relative w-full">
                     <input type="text" #inputFeild1 (keyup.enter)="processData(inputFeild1.value, inputFeild2.value)" class="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400" placeholder="Paste wallet address" required />
@@ -20,20 +20,6 @@ import { FooterComponent } from '../footer/footer.component';
                     <input type="text" #inputFeild2 (keyup.enter)="processData(inputFeild1.value, inputFeild2.value)" class="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400" placeholder="Paste wallet address" required />
                 </div>
                 <button type="submit" (click)="processData(inputFeild1.value, inputFeild2.value)" class="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-light font-mono tracking-widest text-white bg-black rounded-full border focus:outline-none dark:bg-white dark:text-black">Submit</button>
-            </form>
-        </div>
-    </div>
-    <div class="flex justify-center items-center h-1/2 mt-36">
-        <div class="w-full max-w-4xl h-80 bg-slate-100 rounded-md overflow-hidden shadow-xl shadow-slate-300 p-4 dark:bg-zinc-900 dark:shadow-slate-900 dark:shadow-lg dark:text-white">
-            <h2 class="text-3xl text-center font-extralight tracking-widest mb-4 text-black dark:text-gray-200">Contract</h2>
-            <form class="flex items-center flex-col max-w-lg mx-auto mb-4 space-y-10">   
-                <div class="relative w-full">
-                    <input type="text" #inputFeild3 (keyup.enter)="processData(inputFeild1.value, inputFeild2.value)" class="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400" placeholder="Paste wallet contract" required />
-                </div>
-                <div class="relative w-full">
-                    <input type="text" #inputFeild4 (keyup.enter)="processData(inputFeild1.value, inputFeild2.value)"  class="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400" placeholder="Paste wallet contract" required />
-                </div>
-                <button type="submit" (click)="processData(inputFeild3.value, inputFeild4.value)" class="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-light font-mono tracking-widest text-white bg-black rounded-full border focus:outline-none dark:bg-white dark:text-black">Submit</button>
             </form>
         </div>
     </div>
---
+++ src/app/block-data/block-data.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block-data',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      block-data works!
+    </p>
+  `,
+  styleUrl: './block-data.component.scss'
+})
+export class BlockDataComponent {
+
+}
