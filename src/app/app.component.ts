6c045a0d0a986c4916438739f8708751e1156acf
Ramen96 <eric.worsfold@gmail.com>
2024-03-23 20:33:22 -0400

    built card for blockchain data currently on testing route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddressComponent } from './address/address.component';
+import { BlockDataComponent } from './block-data/block-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,AddressComponent],
+  imports: [RouterOutlet, NavbarComponent,AddressComponent, BlockDataComponent],
   template:`
   <app-navbar></app-navbar>
   <router-outlet></router-outlet>
--- src/app/block-data/block-data.component.ts
+++ src/app/block-data/block-data.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-block-data',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   template: `
-    <p>
-      block-data works!
-    </p>
+    <div class="flex justify-center items-center h-1/2 mt-36">
+        <div class="w-full max-w-4xl h-80 bg-slate-100 rounded-md overflow-hidden shadow-xl shadow-slate-300 p-4 dark:bg-zinc-900 dark:shadow-slate-900 dark:shadow-lg dark:text-white">
+            <h2 class="text-3xl text-center font-extralight tracking-widest mb-4 text-black dark:text-gray-200">Blockchain Data</h2>
+        </div>
+    </div>
+    <router-outlet></router-outlet>
   `,
   styleUrl: './block-data.component.scss'
 })
