886f9908aa610b0735d0f450aebfa407b8e7ee6f
Ramen96 <eric.worsfold@gmail.com>
2024-03-24 21:56:40 -0400

    created copyToClipboard method

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -23,7 +23,7 @@ import { FooterComponent } from '../footer/footer.component';
                 <div class="w-full max-w-[32rem]">
                     <div class="relative">
                         <label for="npm-install-copy-button" class="sr-only">Label</label>
-                        <input id="npm-install-copy-button" type="text" class="col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value="Copy to clipboard" disabled readonly>
+                        <input id="npm-install-copy-button" type="text" class="col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value='' disabled readonly>
                         <button data-copy-to-clipboard-target="npm-install-copy-button" data-tooltip-target="tooltip-copy-npm-install-copy-button" class="absolute end-2 top-1/2 -translate-y-1/2 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg p-2 inline-flex items-center justify-center">
                             <span id="default-icon">
                                 <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
@@ -61,8 +61,12 @@ export class AddressComponent {
        ? alert('Please fill out the forms') 
        : (
             (value1 == value2) // If not empty see if they match.
-            ? alert(`It\'s a match you\'re good to go! Copy: ${value1}`) 
+            ? this.copyToClipboard(value1) 
             : alert('WARNING: NOT A MATCH')
         )
+    }
+    copyToClipboard(value1: string) {
+        alert('It\'s a match you\'re good to go!');
+        // Set value1 to a dynamic varuible that will be equal to the value class in the <input> in line 26
     }
 }
